ee38876e87c8a3068550776e5aac87825747771b
Itai Bar-Natan <itaibn3141@gmail.com>
2015-11-09 07:48:26 -0500

    Fixed bug in CoC substitution

--- coc.py
+++ coc.py
@@ -82,6 +82,7 @@ class PairingTerm(Term):
             self.snd_scopes = 1
         else:
             self.snd_scopes = 0
+        self.scopes = scopeQ
 
     def __eq__(self, other):
         return type(self) == type(other) and self.args == other.args
@@ -91,8 +92,20 @@ class PairingTerm(Term):
             + self.snd_scopes))
 
     def subs(self, i, x):
+        """
+        snd_x = x
+        for i in range(self.snd_scopes):
+            snd_x = snd_x.mkfree_var(0)
         return self.remake(self.args[0].subs(i, x), self.args[1].subs(i +
-            self.snd_scopes, x.mkfree_var(0)))
+            self.snd_scopes, snd_x))
+        """
+        if self.snd_scopes == 0:
+            return self.remake(self.args[0].subs(i, x), self.args[1].subs(i, x))
+        elif self.snd_scopes == 1:
+            return self.remake(self.args[0].subs(i, x),
+                               self.args[1].subs(i+1, x.mkfree_var(0)))
+        else:
+            raise AssertionError
 
     def eval(self):
         return self.eval_remake(self.args[0].eval(), self.args[1].eval())
@@ -169,3 +182,12 @@ def std_vargen():
         for v in alphabet:
             yield v + str(n)
         n += 1
+
+def arr(a, b):
+    return ProdType(a, b.mkfree_var(0))
+
+def church(n):
+    expr = Var(0)
+    for _ in range(n):
+        expr = app(Var(1), expr)
+    return Lambda(Sort(0), Lambda(arr(Var(0), Var(0)), Lambda(Var(1), expr)))
